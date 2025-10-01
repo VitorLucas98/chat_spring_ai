b47f84a7a3a119241455a58287056a87ee058f4d
Vitor Lucas <vitorluc.2013@gmail.com>
2025-09-30 22:19:52 -0300

    implementação com VertexAI Gemini

--- src/main/java/com/vbotelho/api_ai/controllers/ChatController.java
+++ src/main/java/com/vbotelho/api_ai/controllers/ChatController.java
@@ -1,6 +1,7 @@
 package com.vbotelho.api_ai.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ai")
 public class ChatController {
-    private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    //private final ChatClient chatClient;
+
+    private final VertexAiGeminiChatModel chatModel;
+
+
+
+//    public ChatController(ChatClient.Builder chatClientBuilder) {
+//        this.chatClient = chatClientBuilder.build();
+//    }
+
+    public ChatController(VertexAiGeminiChatModel chatModel) {
+        this.chatModel = chatModel;
     }
 
-    @GetMapping("/generation")
-    String generation(String userInput) {
-        return this.chatClient.prompt()
-                .user(userInput)
-                .call()
-                .content();
+//    @GetMapping("/generation")
+//    String generation(String userInput) {
+//        return this.chatClient.prompt()
+//                .user(userInput)
+//                .call()
+//                .content();
+//    }
+
+        @GetMapping("/generation")
+        String generation(String userInput) {
+        return this.chatModel.call(userInput);
     }
 }
